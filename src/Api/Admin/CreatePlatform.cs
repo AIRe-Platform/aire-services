c1cdf708ad66bc58ec61ac3c7f90af1efc8701f6
Niko Junnila <niko.junnila@goodlife.technology>
2023-12-07 15:10:07 +0200

    Rework platform configuration API

---
+++ src/Api/Admin/CreatePlatform.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Aire.Helpers;
+using Aire.Services.Models;
+using System.Web.Http;
+using Aire.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+
+namespace Aire.Servces.Api.Admin
+{
+    public class CreateDefaultPlatform
+    {
+        private readonly ILogger<CreateDefaultPlatform> _log;
+        private readonly ITableStorageService _storage;
+
+        public CreateDefaultPlatform(ILogger<CreateDefaultPlatform> log, ITableStorageService storage)
+        {
+            _log = log;
+            _storage = storage;
+        }
+
+        [Function("CreatePlatform")]
+        [OpenApiIgnore]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Admin, "post", Route = "platform/{platform_name}")] HttpRequest req,
+            string platform_name)
+        {
+            PlatformConfiguration config = null;
+            var pk = platform_name;
+            var rk = AireConstants.PlatformConfigRowKey;
+
+            try
+            {
+                using var stream = new StreamReader(req.Body);
+                var configData = await stream.ReadToEndAsync();
+                if(configData.Length > 0)
+                {
+                    config = configData.JsonToObject<PlatformConfiguration>();
+                }
+            }
+            catch(Exception ex)
+            {
+                _log.LogError(ex, "Failed to read request body");
+                return new BadRequestResult();
+            }
+            
+            if(config == null)
+                return new BadRequestResult();
+
+            var ent = new PlatformEntity 
+            {
+                PartitionKey = pk,
+                RowKey = rk,
+                Config = config
+            };
+
+            var result = await _storage.UpsertAsync(ent);
+
+            if(result)
+                return new NoContentResult();
+            else
+                return new InternalServerErrorResult();
+        }
+    }
+}
--- src/Environment.cs
+++ src/Environment.cs
@@ -6,20 +6,16 @@ namespace Aire.Services
             get => System.Environment.GetEnvironmentVariable("StorageConnectionString");
         }
 
-        public static string IDModuleEndpoint { 
-            get => System.Environment.GetEnvironmentVariable("ID_MODULE_ENDPOINT");
-        }
-
-        public static string AIModuleEndpoint {
-            get => System.Environment.GetEnvironmentVariable("AI_MODULE_ENDPOINT");
-        }
-
-        public static string MemoryModuleEndpoint {
-            get => System.Environment.GetEnvironmentVariable("MEMORY_MODULE_ENDPOINT");
-        }
-
         public static string PlatformConfiguration {
-            get => System.Environment.GetEnvironmentVariable("PLATFORM_CONFIGURATION");
+            get => System.Environment.GetEnvironmentVariable("PlatformConfiguration");
+        }
+
+        public static string TokenSigningKey {
+            get => System.Environment.GetEnvironmentVariable("TokenSigningKey");
+        }
+
+        public static string TokenEncryptionKey {
+            get => System.Environment.GetEnvironmentVariable("TokenEncryptionKey");
         }
     }
 }
