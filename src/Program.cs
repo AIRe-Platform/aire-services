46c7681e9a0d85ab772aede271b0b680db716bde
Niko Junnila <niko.junnila@goodlife.technology>
2024-09-25 16:23:55 +0300

    Add missing MPL headers

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
